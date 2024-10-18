cea489731175af092fd5192bf50672b732636a48
yangkun <1269769296@qq.com>
2024-10-19 00:53:58 +0800

    顺滑中间点速度，末速度和下一段初速度设置为两段连线方向向量中线。 仿真有效果

--- src/path/include/path_planning.h
+++ src/path/include/path_planning.h
@@ -26,6 +26,8 @@ public:
 
 
 private:
+    geometry_msgs::Vector3 p1, p2, p3, v1, v2, v_dir, v, current_velocity;
+    double current_V;
     ros::NodeHandle nh;
     ros::Publisher refer_path_cps_pub;
     ros::Subscriber path_points_sub;
@@ -34,6 +36,59 @@ private:
 
     void loadConstraints();
 };
+
+
+
+// a + b
+geometry_msgs::Vector3 add_Vector3(const geometry_msgs::Vector3& a, const geometry_msgs::Vector3& b) {
+    geometry_msgs::Vector3 result;
+    result.x = a.x + b.x;
+    result.y = a.y + b.y;
+    result.z = a.z + b.z;
+    return result;
+}
+
+// a - b
+geometry_msgs::Vector3 subtract_Vector3(const geometry_msgs::Vector3& a, const geometry_msgs::Vector3& b) {
+    geometry_msgs::Vector3 result;
+    result.x = a.x - b.x;
+    result.y = a.y - b.y;
+    result.z = a.z - b.z;
+    return result;
+}
+
+// normalize(a)
+geometry_msgs::Vector3 normalize_Vector3(geometry_msgs::Vector3 a) {
+    // 计算模
+    double norm = sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
+    
+    // 如果模长为0，返回原地向量(0,0,0)避免除以0的错误
+    if (norm == 0) {
+        return geometry_msgs::Vector3();
+    } else {
+        // 归一化向量
+        geometry_msgs::Vector3 unit_vector;
+        unit_vector.x = a.x / norm;
+        unit_vector.y = a.y / norm;
+        unit_vector.z = a.z / norm;
+        return unit_vector;
+    }
+}
+
+// norm(a)
+inline double norm_Vector3(geometry_msgs::Vector3 a) {
+    // 计算模
+    return sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
+}
+
+// scalar * v
+geometry_msgs::Vector3 scalar_multiply_Vector3(const geometry_msgs::Vector3& v, double scalar) {
+    geometry_msgs::Vector3 result;
+    result.x = v.x * scalar;
+    result.y = v.y * scalar;
+    result.z = v.z * scalar;
+    return result;
+}
 
 
 #endif // PATH_PLANNING_H
--- src/path/src/path_planning.cpp
+++ src/path/src/path_planning.cpp
@@ -7,6 +7,11 @@ void PathPlanningNode::pathPointsCallback(const swarm_msgs::MassPoints::ConstPtr
     optimal_paths_msg.num_segment = msg->points.size() - 1; // Number of segments is one less than number of points
     optimal_paths_msg.T_tatal = 0.0;
 
+    current_velocity = msg->points[0].velocity;
+    current_V = norm_Vector3(current_velocity);
+    current_V = std::max(2.0, current_V);   // trick
+
+    // 1. Construct basic information about optimal_paths_msg
     for (size_t i = 0; i < msg->points.size() - 1; ++i) {
         const auto& pt_start = msg->points[i];
         const auto& pt_end = msg->points[i + 1];
@@ -17,6 +22,35 @@ void PathPlanningNode::pathPointsCallback(const swarm_msgs::MassPoints::ConstPtr
         geometry_msgs::Vector3 xT = pt_end.position;
         geometry_msgs::Vector3 vT = pt_end.velocity;
 
+        // Populate TimeOptimalPMMParam message
+        swarm_msgs::TimeOptimalPMMParam path_param;
+        path_param.trajectory_id = i;
+        path_param.x0 = x0;
+        path_param.v0 = v0;
+        path_param.xT = xT;
+        path_param.vT = vT;
+
+        // Add path_param to optimal_paths_msg
+        optimal_paths_msg.pieces.push_back(path_param);
+    }
+
+    // 2. Smooth the speed of the intermediate point
+    for (size_t i = 0; i < msg->points.size() - 2; ++i) {
+        p1 = msg->points[i].position;
+        p2 = msg->points[i+1].position;
+        p3 = msg->points[i+2].position;
+
+        v1 = normalize_Vector3(subtract_Vector3(p2, p1));
+        v2 = normalize_Vector3(subtract_Vector3(p3, p2));
+        v_dir = normalize_Vector3(add_Vector3(v1, v2));
+        v = scalar_multiply_Vector3(v_dir, current_V);
+
+        optimal_paths_msg.pieces[i].vT = v;
+        optimal_paths_msg.pieces[i+1].v0 = v;
+    }
+
+    // 3. Construct the trajectory parameters of optimal_paths_msg
+    for (size_t i = 0; i < optimal_paths_msg.pieces.size(); ++i) {
         // Assuming constant constraints for simplicity
         geometry_msgs::Vector3 umax, umin, vmax, vmin;
         umax.x = u_max[0]; umax.y = u_max[1]; umax.z = u_max[2];
@@ -25,35 +59,32 @@ void PathPlanningNode::pathPointsCallback(const swarm_msgs::MassPoints::ConstPtr
         vmin.x = v_min[0]; vmin.y = v_min[1]; vmin.z = v_min[2];
 
         // Create TimeOptimalPMM instance and compute times
-        TimeOptimalPMM3D pmm3d(x0, v0, xT, vT, umax, umin, vmax, vmin);
-        std::cout << "x0: " << x0 << "\nxT: " << xT << "\nv0: " << v0 << "\nvT: " << vT << std::endl;
+        TimeOptimalPMM3D pmm3d(optimal_paths_msg.pieces[i].x0, 
+                               optimal_paths_msg.pieces[i].v0, 
+                               optimal_paths_msg.pieces[i].xT, 
+                               optimal_paths_msg.pieces[i].vT, 
+                               umax, umin, vmax, vmin);
+        std::cout << "x0: " << optimal_paths_msg.pieces[i].x0 << "\nxT: " << optimal_paths_msg.pieces[i].xT << "\nv0: " << optimal_paths_msg.pieces[i].v0 << "\nvT: " << optimal_paths_msg.pieces[i].vT << std::endl;
         auto [t1, t2, T, case_idx] = pmm3d.compute_times();
         // Synchronized u
         umax.x = pmm3d.pmm_x.umax; umax.y = pmm3d.pmm_y.umax; umax.z = pmm3d.pmm_z.umax;
         umin.x = pmm3d.pmm_x.umin; umin.y = pmm3d.pmm_y.umin; umin.z = pmm3d.pmm_z.umin;
 
-        // Populate TimeOptimalPMMParam message
-        swarm_msgs::TimeOptimalPMMParam path_param;
-        path_param.trajectory_id = i;
-        path_param.x0 = x0;
-        path_param.v0 = v0;
-        path_param.xT = xT;
-        path_param.vT = vT;
-        path_param.umax = umax;
-        path_param.umin = umin;
-        path_param.t1 = t1;
-        path_param.t2 = t2;
-        path_param.T = T;
-        path_param.case_idx = case_idx;
+        // Update TimeOptimalPMMParam message
+        optimal_paths_msg.pieces[i].umax = umax;
+        optimal_paths_msg.pieces[i].umin = umin;
+        optimal_paths_msg.pieces[i].t1 = t1;
+        optimal_paths_msg.pieces[i].t2 = t2;
+        optimal_paths_msg.pieces[i].T = T;
+        optimal_paths_msg.pieces[i].case_idx = case_idx;
         optimal_paths_msg.T_tatal += T;
-
-        // Add path_param to optimal_paths_msg
-        optimal_paths_msg.pieces.push_back(path_param);
     }
 
+    // 4. Publish
     refer_path_cps_pub.publish(optimal_paths_msg);
 }
 
+// Run at construction
 void PathPlanningNode::loadConstraints() {
     try {
         // Load YAML file
